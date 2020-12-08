65e6162b474a2618b18ec7541910626749763f0e
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2020-12-08 18:46:45 +0900

    その他の絞り込み部分を実装

---
+++ src/components/styling/browse/FilterMediaGroup.tsx
@@ -0,0 +1,67 @@
+import {
+  Checkbox,
+  FormControlLabel,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+} from "@material-ui/core";
+import React from "react";
+import FilterMediaResponse from "../../../model/api/response/styling/browse/FilterMediaResponse";
+import Filter from "../../../model/styling/browse/Filter";
+
+interface FilterMediaGroupProps {
+  labelIdPrefix: string;
+  data: FilterMediaResponse[];
+  selected: Filter[];
+  callback: (filter: Filter) => () => void;
+}
+
+const FilterMediaGroup = (props: FilterMediaGroupProps) => {
+  return (
+    <List>
+      {props.data.map((row) => {
+        const labelId = props.labelIdPrefix + row.id;
+        return (
+          <ListItem
+            key={row.id}
+            role={undefined}
+            dense
+            button
+            onClick={props.callback({
+              id: row.id,
+              name: row.name,
+            })}
+          >
+            <ListItemIcon>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={
+                      props.selected.findIndex((item) => item.id === row.id) !==
+                      -1
+                    }
+                    tabIndex={-1}
+                    disableRipple
+                    inputProps={{ "aria-labelledby": labelId }}
+                  />
+                }
+                label={
+                  <img
+                    src={"https://leeap.jp/" + row.imagePath}
+                    width="40px"
+                    height="auto"
+                    alt=""
+                  />
+                }
+              />
+            </ListItemIcon>
+            <ListItemText>{row.name}</ListItemText>
+          </ListItem>
+        );
+      })}
+    </List>
+  );
+};
+
+export default FilterMediaGroup;
