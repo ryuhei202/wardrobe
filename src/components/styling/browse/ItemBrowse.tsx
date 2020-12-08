bdfbe070db3383adb2c54391c9e1d60dd9bfb1ae
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2020-12-08 18:33:08 +0900

    Browseコンポネントを追加

--- src/components/styling/browse/ItemBrowse.tsx
+++ src/components/styling/browse/ItemBrowse.tsx
@@ -1,7 +1,81 @@
+import {
+  CircularProgress,
+  FormControl,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import { useGetRefinementChoiceCaller } from "../../../model/styling/browse/api_caller/UseGetRefinementChoiceCaller";
+import AppliedFilters from "./AppliedFilters";
+import FilterGroupCollection from "./FilterGroupCollection";
+import { useItemBrowseHandler } from "./handler/UseItemBrowseHandler";
+import ItemCardCollection from "./ItemCardCollection";
+import { useBrowseStyle } from "./style/UseBrowseStyle";
 
 const ItemBrowse = () => {
-  return <></>;
+  const classes = useBrowseStyle();
+
+  const choiceApiCaller = useGetRefinementChoiceCaller();
+  const handler = useItemBrowseHandler();
+
+  if (choiceApiCaller.isRunning()) {
+    return <CircularProgress />;
+  } else if (choiceApiCaller.errorResponse !== null) {
+    return <Typography>{choiceApiCaller.errorResponse.message}</Typography>;
+  } else if (choiceApiCaller.response !== null) {
+    let searchCount;
+    let searchResult = handler.searchResult();
+    if (searchResult) {
+      searchCount = searchResult.totalCount;
+    } else {
+      searchCount = 0;
+    }
+
+    return (
+      <>
+        <Typography variant="h6" noWrap>
+          アイテム一覧
+        </Typography>
+        <Toolbar>
+          <div className={classes.filterPaper}>
+            <Typography paragraph={true}>
+              検索結果
+              <br />
+              {searchCount}件
+            </Typography>
+          </div>
+          <div className={classes.appliedFilterContainer}>
+            <Typography>適用済みフィルター</Typography>
+            <AppliedFilters
+              data={handler.currentRefinement}
+              callback={handler.appliedFiltersCallback}
+            />
+          </div>
+          <div>
+            <Typography>並べ替え</Typography>
+            <FormControl className={classes.sortSelection}>
+              <Select native defaultValue={1} onChange={handler.onSortChanged}>
+                {choiceApiCaller.response.sort.map((row) => (
+                  <option value={row.id}>{row.name}</option>
+                ))}
+              </Select>
+            </FormControl>
+          </div>
+        </Toolbar>
+        <div className={classes.searchField}>
+          <FilterGroupCollection
+            data={choiceApiCaller.response.filter}
+            callback={handler.filterGroupCollectionCallback}
+            refinement={handler.currentRefinement}
+          />
+          <ItemCardCollection data={handler.searchResult()?.content ?? null} />
+        </div>
+      </>
+    );
+  } else {
+    return <></>;
+  }
 };
 
 export default ItemBrowse;
