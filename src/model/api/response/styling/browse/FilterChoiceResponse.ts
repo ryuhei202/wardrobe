6c1ca015a7a932fef54a77a8b360fb054e6ddd5c
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2020-12-25 15:19:46 +0900

    APIの改修に対応

--- src/model/api/response/styling/browse/FilterChoiceResponse.ts
+++ src/model/api/response/styling/browse/FilterChoiceResponse.ts
@@ -1,10 +1,12 @@
 import FilterMediaResponse from "./FilterMediaResponse";
+import FilterRangeResponse from "./FilterRangeResponse";
 import FilterResponse from "./FilterResponse";
 import LargeCategoryChoiceResponse from "./LargeCategoryChoiceResponse";
 
 export default interface FilterChoiceResponse {
   readonly largeCategory: LargeCategoryChoiceResponse[];
   readonly size: FilterResponse[];
+  readonly partSize: FilterRangeResponse[];
   readonly color: FilterMediaResponse[];
   readonly pattern: FilterMediaResponse[];
   readonly logo: FilterMediaResponse[];
