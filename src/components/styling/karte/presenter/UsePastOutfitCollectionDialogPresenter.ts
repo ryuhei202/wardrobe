ad46a9fabe2d15f3a7db781ede4aa550ac6c6183
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2021-01-04 13:16:27 +0900

    過去コーデダイアログコンポネントの実装

---
+++ src/components/styling/karte/presenter/UsePastOutfitCollectionDialogPresenter.ts
@@ -0,0 +1,31 @@
+import PastOutfitItemResponse from "../../../../model/api/response/styling/karte/PastOutfitItemResponse";
+import PastOutfitResponse from "../../../../model/api/response/styling/karte/PastOutfitResponse";
+
+export interface PastOutfitCollectionDialogPresenter {
+  shipmentDate: (index: number) => string;
+  coordinateFeedback: (index: number) => string[];
+  itemListSecondary: (item: PastOutfitItemResponse) => string;
+}
+
+export const usePastOutfitCollectionDialogPresenter = (
+  data: PastOutfitResponse[]
+): PastOutfitCollectionDialogPresenter => {
+  const shipmentDate = (index: number): string =>
+    data[index].rentalStartedAt
+      ? new Date(data[index].rentalStartedAt!).toLocaleDateString()
+      : "";
+
+  const coordinateFeedback = (index: number): string[] => {
+    return data[index].feedback.split("\n");
+  };
+
+  const itemListSecondary = (item: PastOutfitItemResponse): string => {
+    let result = `${item.size}`;
+    item.partSizes.forEach((partSize) => {
+      result += `, ${partSize.name}: ${partSize.value ?? "未計測"}`;
+    });
+    return result;
+  };
+
+  return { shipmentDate, coordinateFeedback, itemListSecondary };
+};
