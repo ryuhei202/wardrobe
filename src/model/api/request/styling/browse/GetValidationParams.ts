03a54c44e3802f33ef6fda9661ec2958aaa8e40e
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2021-01-19 12:37:55 +0900

    API周りを実装

---
+++ src/model/api/request/styling/browse/GetValidationParams.ts
@@ -0,0 +1,4 @@
+export default interface GetValidationParams {
+  chartId: number;
+  itemId: number;
+}
---
+++ src/model/styling/browse/api_caller/UseGetValidationCaller.ts
@@ -0,0 +1,54 @@
+import { CallStatus } from "../../../api/shared/CallStatus";
+import { useGetClient } from "../../../api/client/UseGetClient";
+import { useEffect, useState } from "react";
+import ErrorResponse from "../../../api/response/shared/ErrorResponse";
+import { ChartId } from "../../../ChartId";
+import ValidationResponse from "../../../api/response/styling/browse/ValidationResponse";
+import { useGetValidationRequest } from "../../../api/request/styling/browse/UseGetValidationRequest";
+
+export interface GetValidationCaller {
+  isRunning: () => boolean;
+  response: ValidationResponse[] | null;
+  errorResponse: ErrorResponse | null;
+}
+
+export const useGetValidationCaller = (
+  itemId: number,
+  onSuccess: (response: ValidationResponse[]) => void
+): GetValidationCaller => {
+  const [response, setResponse] = useState<ValidationResponse[] | null>(null);
+  const [callStatus, setCallStatus] = useState(CallStatus.Preparing);
+  const [errorResponse, setErrorResponse] = useState<ErrorResponse | null>(
+    null
+  );
+
+  const request = useGetValidationRequest(ChartId(), itemId);
+  const client = useGetClient<ValidationResponse[]>(request);
+
+  useEffect(() => {
+    if (callStatus === CallStatus.Preparing) {
+      const fetch = () => {
+        client
+          .get()
+          .then((response) => {
+            setResponse(response);
+            setErrorResponse(null);
+            setCallStatus(CallStatus.Idle);
+            onSuccess(response);
+          })
+          .catch((error: ErrorResponse) => {
+            setErrorResponse(error);
+            setCallStatus(CallStatus.Idle);
+          });
+      };
+      setCallStatus(CallStatus.Running);
+      fetch();
+    }
+  }, [callStatus, client, onSuccess]);
+
+  const isRunning = (): boolean => {
+    return callStatus === CallStatus.Running;
+  };
+
+  return { isRunning, response, errorResponse };
+};
