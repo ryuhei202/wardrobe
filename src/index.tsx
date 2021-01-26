0f97f86396a4cbcb3f2f4eaaaea8d71cb323dfc9
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2021-01-26 17:41:01 +0900

    firebase SDKをインストール

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
+import firebase from "firebase/app";
+import "firebase/analytics";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDKEewcjjMpq4XYPRxbtmsLSOEYwlHCijA",
+  authDomain: "leeap-wardrobe.firebaseapp.com",
+  databaseURL: "https://leeap-wardrobe.firebaseio.com",
+  projectId: "leeap-wardrobe",
+  storageBucket: "leeap-wardrobe.appspot.com",
+  messagingSenderId: "795958516059",
+  appId: "1:795958516059:web:41989e1d2bb26a7ca5627e",
+  measurementId: "G-PN1JLN0FQH",
+});
+firebase.analytics();
 
 ReactDOM.render(
   <React.StrictMode>
