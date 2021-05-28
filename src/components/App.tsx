ce1ad1804ca57f6dbbf609a7c544bc2e86308682
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2021-05-28 17:14:00 +0900

    QRコードを設置

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,24 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar/AppBar";
-import { Toolbar, Typography } from "@material-ui/core";
+import { Button, Dialog, Toolbar, Typography } from "@material-ui/core";
 import DefaultTheme from "./style/Theme";
 import { useAppStyle } from "./style/UseAppStyle";
 import Styling from "./styling/Styling";
 import { ThemeProvider } from "@material-ui/core/styles";
+import QRCode from "react-qr-code";
+import { ChartId } from "../model/ChartId";
+import { CropFree } from "@material-ui/icons";
 
 const App = () => {
   const classes = useAppStyle();
+
+  const [isQRCodeOpen, setIsQRCodeOpen] = useState(false);
 
   return (
     <ThemeProvider theme={DefaultTheme}>
       <div className={classes.root}>
         <AppBar position="fixed" className={classes.appBar}>
           <Toolbar>
-            <Typography variant="h6" noWrap>
+            <Typography variant="h6" noWrap className={classes.title}>
               WARDROBE
             </Typography>
+            <span className={classes.qrCode}>
+              <Button
+                variant="contained"
+                color="primary"
+                startIcon={<CropFree />}
+                onClick={() => setIsQRCodeOpen(true)}
+              >
+                コーデピック
+              </Button>
+            </span>
           </Toolbar>
         </AppBar>
+        <Dialog open={isQRCodeOpen} onClose={() => setIsQRCodeOpen(false)}>
+          <QRCode value={ChartId().toString()} size={300} />
+        </Dialog>
         <Styling />
       </div>
     </ThemeProvider>
