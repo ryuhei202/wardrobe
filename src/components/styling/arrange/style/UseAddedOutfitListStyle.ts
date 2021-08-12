ac4c72185769f47599d786a90a59828abf7b9af2
Hiroaki Iwai <h-iwai@kiizan-kiizan.co.jp>
2021-08-12 17:51:18 +0900

    Arrangeコンポーネントまわりのスタイル切り分け

---
+++ src/components/styling/arrange/style/UseAddedOutfitListStyle.ts
@@ -0,0 +1,9 @@
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+
+export const useAddedOutfitListStyle = makeStyles((theme: Theme) =>
+  createStyles({
+    registeredAdviceListContainer: {
+      marginTop: theme.spacing(1),
+    },
+  })
+);
