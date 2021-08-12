f143b8c8ce77320233b2109f33eac9cbd3b66577
Hiroaki Iwai <h-iwai@kiizan-kiizan.co.jp>
2021-08-12 18:10:33 +0900

    使ってない定義を削除

--- src/components/styling/karte/style/UseKarteStyle.ts
+++ src/components/styling/karte/style/UseKarteStyle.ts
@@ -2,27 +2,8 @@ import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 export const useKarteStyle = makeStyles((theme: Theme) =>
   createStyles({
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular,
-    },
     drawerList: {
       maxWidth: 480,
-    },
-    accordionDetails: {
-      display: "block",
-    },
-    memberImageCollectionImage: {
-      top: "50%",
-      position: "relative",
-      transform: "translateY(-50%)",
-    },
-    tabContainer: {
-      flexGrow: 1,
-      maxWidth: 480,
-    },
-    contentContainer: {
-      overflow: "auto",
     },
   })
 );
