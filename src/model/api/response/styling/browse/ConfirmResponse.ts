56f3efbae4f439c575a0d141079312f1ab438902
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2021-11-30 13:18:35 +0900

    登録済みのコーデ作成者をデフォルト値に設定

--- src/model/api/response/styling/browse/ConfirmResponse.ts
+++ src/model/api/response/styling/browse/ConfirmResponse.ts
@@ -3,6 +3,9 @@ import { ValidationError } from "../../../../styling/browse/ValidationError";
 
 export interface ConfirmResponse {
   readonly validateErrors: ValidationError[];
-  readonly stylistChoice: SelectChoiceResponse[];
+  readonly stylistInfo: {
+    selectChoice: SelectChoiceResponse[];
+    selectedId: number | null;
+  };
   readonly misplacedItems: string[];
 }
