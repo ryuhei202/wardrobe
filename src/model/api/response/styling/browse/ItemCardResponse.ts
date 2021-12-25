b3eee583d3f2786e2eb8108a9cac82de5f05c41c
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2021-12-25 18:03:13 +0900

    modelにseriesFeatureを追加

--- src/model/api/response/styling/browse/ItemCardResponse.ts
+++ src/model/api/response/styling/browse/ItemCardResponse.ts
@@ -2,7 +2,8 @@ export interface ItemCardResponse {
   readonly id: number;
   readonly mainColorImagePath: string;
   readonly subColorImagePath: string;
-  readonly seriesName: string;
+  readonly seriesName: string | null;
+  readonly seriesFeature: string | null;
   readonly categoryName: string;
   readonly brandName: string;
   readonly imagePath: string;
