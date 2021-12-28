5764e5e9b2457c13009fbaa031d41ae728ee854a
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2021-12-28 15:30:26 +0900

    返り値の型をつけ忘れを修正

--- src/hooks/api/UsePurchasedItemsIndex.ts
+++ src/hooks/api/UsePurchasedItemsIndex.ts
@@ -1,7 +1,11 @@
 import { PurchasedItemIndexResponse } from "../../model/api/response/styling/purchaseItem/PurchasedItemIndexResponse";
 import { useMemberGetRequest } from "./UseMemberGetRequest";
 
-export const usePurchasedItemsIndex = () => {
+type PurchasedItemsIndex = {
+  readonly data?: PurchasedItemIndexResponse[];
+  readonly error: Error | null;
+};
+export const usePurchasedItemsIndex = (): PurchasedItemsIndex => {
   const { data, error } = useMemberGetRequest<PurchasedItemIndexResponse[]>(
     "purchased_items"
   );
