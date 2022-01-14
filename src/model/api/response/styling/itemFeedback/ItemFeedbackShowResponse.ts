ce33a06912683ab56ae02a2dd73698fe396aabc7
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-01-14 16:58:25 +0900

    レスポンスのモデルを作成

--- src/model/api/response/styling/itemFeedback/ItemFeedbackShowResponse.ts
+++ src/model/api/response/styling/itemFeedback/ItemFeedbackShowResponse.ts
@@ -1,4 +1,7 @@
+import { ImagePathResponse } from "../ImagePathResponse";
+
 export interface ItemFeedbackShowResponse {
   readonly chartItemId: number;
+  readonly imagePath: ImagePathResponse;
   readonly textFeedback: string;
 }
