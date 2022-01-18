b3edda2bac45f676c678893d9fa4d225783e1caf
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-01-18 09:18:08 +0900

    成功、失敗時にsnackbarを表示する処理を実装

--- src/components/feedback/Feedbacks.tsx
+++ src/components/feedback/Feedbacks.tsx
@@ -1,18 +1,20 @@
-import { Box, Typography } from "@mui/material";
+import { Alert, Box, Snackbar, Typography } from "@mui/material";
 import { ItemFeedbackShowResponse } from "../../model/api/response/styling/itemFeedback/ItemFeedbackShowResponse";
 import { FeedbackForm } from "./FeedbackForm";
+import { useFeedbacksHandler } from "./handler/UseFeedbacksHandler";
 
 type Props = {
   readonly response: ItemFeedbackShowResponse[];
 };
 export const Feedbacks = (props: Props) => {
+  const handler = useFeedbacksHandler();
   return (
     <>
       <Typography variant="body1" fontWeight="bold" m={2}>
         フィードバック
       </Typography>
       <Typography variant="body2" m={3}>
-        ※Alt(Option) + Enter または Ctrl(Command) + Enterで保存
+        ※Alt(Option) + Enterで保存
       </Typography>
       <Box
         component="form"
@@ -22,8 +24,15 @@ export const Feedbacks = (props: Props) => {
         }}
       >
         {props.response.map((itemFeedback) => (
-          <FeedbackForm key={itemFeedback.chartItemId} data={itemFeedback} />
+          <FeedbackForm
+            key={itemFeedback.chartItemId}
+            data={itemFeedback}
+            callback={handler.feedbackFormCallback()}
+          />
         ))}
+        <Snackbar open={handler.isSnackBarOpen}>
+          <Alert severity={handler.severity}>{handler.snackBarText}</Alert>
+        </Snackbar>
       </Box>
     </>
   );
