9e1169ddb0312c976ed2f71bd9d4f771432120f7
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-01-27 18:16:42 +0900

    保存終了後にsnackbarを表示する

--- src/components/hearing/HearingForm.tsx
+++ src/components/hearing/HearingForm.tsx
@@ -2,13 +2,15 @@ import { Box, TextField } from "@mui/material";
 import { useState } from "react";
 import { useLatestStylingReferenceTextsUpdate } from "../../hooks/api/UseLatestStylingReferenceTextsUpdate";
 import { SendButton } from "../shared/SendButton";
+import { HearingFormCallback } from "./callback/HearingFormCallback";
 import { useHearingFormHandler } from "./handler/useHearingFormHandler";
 
 type Props = {
   readonly category: { id: number; title: string };
+  readonly callback: HearingFormCallback;
   readonly initialText: string;
 };
-export const HearingForm = ({ category, initialText }: Props) => {
+export const HearingForm = ({ category, callback, initialText }: Props) => {
   const [referenceText, setReferenceText] = useState<string>(initialText);
   const [isEditing, setIsEditing] = useState<boolean>(false);
 
@@ -22,6 +24,7 @@ export const HearingForm = ({ category, initialText }: Props) => {
     handleCaller,
     handleKeyDown,
   } = useHearingFormHandler(
+    callback,
     initialText,
     setReferenceText,
     isEditing,
