1a8dbe862d546f0aeb74f454bb950194b22799c3
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-01-31 10:37:41 +0900

    snackbarのattributeを使って表示を管理する

--- src/components/hearing/HearingLayout.tsx
+++ src/components/hearing/HearingLayout.tsx
@@ -1,4 +1,5 @@
 import { Alert, Box, Snackbar, Typography } from "@mui/material";
+import { useState } from "react";
 import { StylingReferenceText } from "../../model/hearing/StylingReferenceText";
 import { useHearingLayoutHandler } from "./handler/useHearingLayoutHandler";
 import { HearingForm } from "./HearingForm";
@@ -7,18 +8,25 @@ type Props = {
   readonly response: StylingReferenceText[];
 };
 export const HearingLayout = ({ response }: Props) => {
+  const [severity, setSeverity] = useState<"success" | "error" | undefined>(
+    undefined
+  );
+  const [isSnackBarOpen, setIsSnackBarOpen] = useState<boolean>(false);
+  const [snackBarText, setSnackBarText] = useState<string | undefined>(
+    undefined
+  );
   //現在ヒアリングしているカテゴリ
   const ACTIVE_CATEGORIES = [
     { id: 1, title: "意識する相手(補足)" },
     { id: 7, title: "コーデイメージ" },
     { id: 8, title: "その他" },
   ];
-  const {
-    severity,
-    isSnackBarOpen,
-    snackBarText,
-    hearingFormCallback,
-  } = useHearingLayoutHandler();
+  const { hearingFormCallback } = useHearingLayoutHandler(
+    setSeverity,
+    setIsSnackBarOpen,
+    setSnackBarText
+  );
+
   return (
     <>
       <Typography variant="body1" fontWeight="bold" m={2}>
@@ -43,7 +51,11 @@ export const HearingLayout = ({ response }: Props) => {
             }
           />
         ))}
-        <Snackbar open={isSnackBarOpen}>
+        <Snackbar
+          open={isSnackBarOpen}
+          autoHideDuration={5000}
+          onClose={() => setIsSnackBarOpen(false)}
+        >
           <Alert severity={severity}>{snackBarText}</Alert>
         </Snackbar>
       </Box>
