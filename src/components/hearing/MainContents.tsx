b82a0685a6838276b3335e4d55a5352b4c0309e9
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-02-01 17:52:52 +0900

    各フォームをタブで切り替えるよう改修

--- src/components/hearing/MainContents.tsx
+++ src/components/hearing/MainContents.tsx
@@ -1,13 +1,30 @@
-import { Divider } from "@mui/material";
+import { Box, Tab, Tabs, Typography } from "@mui/material";
+import React, { useState } from "react";
 import { FeedbacksContainer } from "../feedback/FeedbacksContainer";
+import { MemberMemoContainer } from "../memberMemo/MemberMemoContainer";
 import { HearingLayoutContainer } from "./HearingLayoutContainer";
 
+const TABS = [
+  { label: "フィードバック", content: <FeedbacksContainer /> },
+  { label: "ヒアリング", content: <HearingLayoutContainer /> },
+  { label: "メモ", content: <MemberMemoContainer /> },
+];
+
 export const MainContents = () => {
+  const [tabIndex, setTabIndex] = useState(0);
   return (
     <>
-      <FeedbacksContainer />
-      <Divider style={{ margin: 20 }} />
-      <HearingLayoutContainer />
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs value={tabIndex} onChange={(_event, value) => setTabIndex(value)}>
+          {TABS.map((tab) => (
+            <Tab label={tab.label} />
+          ))}
+        </Tabs>
+      </Box>
+      <Typography variant="body2" m={3}>
+        ※Alt(Option) + Enterで保存
+      </Typography>
+      {TABS[tabIndex].content}
     </>
   );
 };
