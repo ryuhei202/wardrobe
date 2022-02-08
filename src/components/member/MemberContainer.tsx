a7e49a79ef26599295526252f8138d4ab45b0899
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-02-08 19:43:19 +0900

    IsMarriagePlanContextを追加

--- src/components/member/MemberContainer.tsx
+++ src/components/member/MemberContainer.tsx
@@ -1,11 +1,20 @@
 import { CircularProgress, Typography } from "@mui/material";
+import React from "react";
 import { useMembersShow } from "../../hooks/api/UseMembersShow";
 import { Member } from "./Member";
+
+export const IsMarriagePlanContext = React.createContext<boolean | undefined>(
+  undefined
+);
 
 export const MemberContainer = () => {
   const { data, error } = useMembersShow();
 
   if (!data) return <CircularProgress />;
   if (error) return <Typography>{error.message}</Typography>;
-  return <Member response={data} />;
+  return (
+    <IsMarriagePlanContext.Provider value={data.isMarriagePlan}>
+      <Member response={data} />
+    </IsMarriagePlanContext.Provider>
+  );
 };
