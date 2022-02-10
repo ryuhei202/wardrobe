1e129e4ef5015a0f3a7a481d544f132d82b48e9a
SoTakano <s-takano@kiizan-kiizan.co.jp>
2022-02-10 20:51:02 +0900

    SelectingContainerでmemberShowをContextに入れるように

--- src/components/selecting/SelectingContainer.tsx
+++ src/components/selecting/SelectingContainer.tsx
@@ -1,11 +1,34 @@
 import { CircularProgress, Typography } from "@mui/material";
+import { useContext, useEffect } from "react";
 import { useKartesShow } from "../../hooks/api/UseKartesShow";
+import { useMembersShow } from "../../hooks/api/UseMembersShow";
+import {
+  ChartIdContext,
+  MemberIdContext,
+  MemberShowContext,
+} from "../context/provider/ContextProvider";
 import { Selecting } from "./Selecting";
 
 export const SelectingContainer = () => {
-  const { data, error } = useKartesShow();
+  const chartId = useContext(ChartIdContext).state!;
+  const memberId = useContext(MemberIdContext).state!;
+  const setMemberShowContext = useContext(MemberShowContext).setter;
 
-  if (!data) return <CircularProgress />;
-  if (error) return <Typography>{error.message}</Typography>;
-  return <Selecting response={data} />;
+  const { data: karteShowData, error: karteShowError } = useKartesShow({
+    chartId,
+  });
+
+  const memberShowRes = useMembersShow({
+    memberId,
+  });
+
+  useEffect(() => {
+    setMemberShowContext(memberShowRes);
+  }, [memberShowRes.data?.id, setMemberShowContext]); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (!karteShowData) return <CircularProgress sx={{ m: "auto" }} />;
+  if (karteShowError)
+    return <Typography sx={{ m: "auto" }}>{karteShowError.message}</Typography>;
+
+  return <Selecting karteShowResponse={karteShowData} />;
 };
