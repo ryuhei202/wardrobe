0cd95860e61424d6d969808790ef43bf9e1f3c43
SoTakano <s-takano@kiizan-kiizan.co.jp>
2022-02-10 19:35:25 +0900

    ContextProviderの実装

---
+++ src/components/provider/ContextProvider.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode, useState } from "react";
+import { TMembersShow } from "../../hooks/api/UseMembersShow";
+
+type TContext<T> = {
+  state: T;
+  setter: React.Dispatch<T>;
+};
+
+export const ChartIdContext = React.createContext<TContext<null | number>>({
+  state: null,
+  setter: () => {},
+});
+
+export const MemberIdContext = React.createContext<TContext<null | number>>({
+  state: null,
+  setter: () => {},
+});
+
+export const MemberShowContext = React.createContext<
+  TContext<null | TMembersShow>
+>({
+  state: null,
+  setter: () => {},
+});
+
+type TProps = {
+  children: ReactNode;
+};
+
+export const ContextProvider = ({ children }: TProps) => {
+  const [chartId, setChartId] = useState<null | number>(null);
+  const [memberId, setMemberId] = useState<null | number>(null);
+  const [memberShow, setMemberShow] = useState<null | TMembersShow>(null);
+
+  return (
+    <MemberShowContext.Provider
+      value={{ state: memberShow, setter: setMemberShow }}
+    >
+      <MemberIdContext.Provider
+        value={{ state: memberId, setter: setMemberId }}
+      >
+        <ChartIdContext.Provider value={{ state: chartId, setter: setChartId }}>
+          {children}
+        </ChartIdContext.Provider>
+      </MemberIdContext.Provider>
+    </MemberShowContext.Provider>
+  );
+};
