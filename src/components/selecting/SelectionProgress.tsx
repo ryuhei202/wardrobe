49d83d380c61645c963738754bfe03b221d399fe
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-03-01 18:13:49 +0900

    ダミー

--- src/components/selecting/SelectionProgress.tsx
+++ src/components/selecting/SelectionProgress.tsx
@@ -34,7 +34,6 @@ export const SelectionProgress = (props: SelectionProgressProps) => {
   }>(initialState);
 
   let steps = [];
-
   for (let index = 0; index < props.data.rentableItemNum; index++) {
     let stepLabel;
     if (props.data.items.length > index) {
