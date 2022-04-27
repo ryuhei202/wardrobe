37b6e462489bcd8c83f286c94944f20b0741bb91
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-04-27 18:48:00 +0900

    includeは必須だったので追加

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
       org: process.env.SENTRY_ORG,
       project: process.env.SENTRY_PROJECT,
       authToken: process.env.SENTRY_AUTH_TOKEN,
-      release: process.env.SENTRY_RELEASE,
+      include: ".",
     }),
   ],
 };
