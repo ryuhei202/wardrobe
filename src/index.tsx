38bad8ada915c815aacf776dbb00d191b7f00b0a
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-05-09 11:21:31 +0900

    エラーボタンを削除

--- src/index.tsx
+++ src/index.tsx
@@ -19,13 +19,6 @@ ReactDOM.render(
     <BrowserRouter>
       <Sentry.ErrorBoundary>
         <App />
-        <button
-          onClick={() => {
-            throw new Error("えらあああ");
-          }}
-        >
-          エラー
-        </button>
       </Sentry.ErrorBoundary>
     </BrowserRouter>
   </React.StrictMode>,
