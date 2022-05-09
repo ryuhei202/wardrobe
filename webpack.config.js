6ff2e9127fcd51faf4eb518d4dfceb9f9046c807
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-05-09 11:21:15 +0900

    source-mapに使用するように変更

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 import SentryPlugin from "@sentry/webpack-plugin";
 
 module.exports = {
-  devtool: "hidden-source-map",
+  devtool: "source-map",
   plugins: [
     new SentryPlugin({
       org: process.env.REACT_APP_SENTRY_ORG,
