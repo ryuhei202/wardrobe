fa6e025634c6e1e8314dc70839a168563bf6bac6
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-05-19 17:39:12 +0900

    feat: タブを使ってなかったので削除して会員写真登録ボタンを追加

--- src/components/hearing/MainContents.tsx
+++ src/components/hearing/MainContents.tsx
@@ -1,28 +1,32 @@
-import { Box, Tab, Tabs, Typography } from "@mui/material";
+import { Box, Button, Tab, Tabs, Typography } from "@mui/material";
 import React, { useState } from "react";
+import { HostUrl } from "../../model/HostUrl";
+import { MemberIdContext } from "../context/provider/ContextProvider";
+import { useContextDefinedState } from "../context/UseContextDefinedState";
 import { MemberMemoContainer } from "../memberMemo/MemberMemoContainer";
 
-const TABS = [
-  // TODO: 必要になったらコメントアウト { label: "フィードバック", content: <FeedbacksContainer /> },
-  // TODO: 必要になったらコメントアウト { label: "ヒアリング", content: <HearingLayoutContainer /> },
-  { label: "メモ", content: <MemberMemoContainer /> },
-];
-
 export const MainContents = () => {
-  const [tabIndex, setTabIndex] = useState(0);
+  const memberPhotoSUPath = `${HostUrl()}/igoue_admin/members/${useContextDefinedState(
+    MemberIdContext
+  )}/member_photos`;
+
   return (
     <>
-      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-        <Tabs value={tabIndex} onChange={(_event, value) => setTabIndex(value)}>
-          {TABS.map((tab) => (
-            <Tab label={tab.label} />
-          ))}
-        </Tabs>
-      </Box>
+      <div>
+        <Button
+          href={memberPhotoSUPath}
+          target="_blank"
+          variant="contained"
+          color="secondary"
+          style={{ float: "right" }}
+        >
+          会員写真登録
+        </Button>
+      </div>
       <Typography variant="body2" m={3}>
         ※Alt(Option) + Enterで保存
       </Typography>
-      {TABS[tabIndex].content}
+      <MemberMemoContainer />
     </>
   );
 };
