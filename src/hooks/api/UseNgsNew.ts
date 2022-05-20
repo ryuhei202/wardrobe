18d5b75dd2b065d9312fbab6f314e31283a2f456
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-05-20 13:08:57 +0900

    feat: ngs#newの型を修正

--- src/hooks/api/UseNgsNew.ts
+++ src/hooks/api/UseNgsNew.ts
@@ -1,8 +1,8 @@
 import { NgNewResponse } from "../../model/api/response/styling/ng/NgNewResponse";
 import { useMemberGetRequest } from "./UseMemberGetRequest";
 
-type NgsNew = {
-  readonly data?: NgNewResponse[];
+type TNgsNew = {
+  readonly data?: NgNewResponse;
   readonly error: Error | null;
 };
 
@@ -10,8 +10,8 @@ type TNgsNewArg = {
   memberId: number;
 };
 
-export const useNgsNew = ({ memberId }: TNgsNewArg): NgsNew => {
-  const { data, error } = useMemberGetRequest<NgNewResponse[], undefined>(
+export const useNgsNew = ({ memberId }: TNgsNewArg): TNgsNew => {
+  const { data, error } = useMemberGetRequest<NgNewResponse, undefined>(
     "ngs",
     memberId
   );
