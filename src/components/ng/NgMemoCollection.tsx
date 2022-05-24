53002210e4034c083858412dfee8b4bbe2427cf0
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-05-24 09:17:41 +0900

    feat: NGメモの削除機能を実装

--- src/components/ng/NgMemoCollection.tsx
+++ src/components/ng/NgMemoCollection.tsx
@@ -1,32 +1,63 @@
+import { Delete } from "@mui/icons-material";
 import {
+  Alert,
   Avatar,
+  IconButton,
   List,
   ListItem,
   ListItemAvatar,
   ListItemText,
   ListSubheader,
+  Snackbar,
 } from "@mui/material";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
+import { useQueryClient } from "react-query";
+import { useNgsDestroy } from "../../hooks/api/UseNgsDestroy";
 import { NgIndexResponse } from "../../model/api/response/styling/ng/NgIndexResponse";
 import { PopupImage } from "../shared/PopupImage";
 
 type Props = {
   readonly response: NgIndexResponse[];
 };
-
 export const NgMemoCollection = (props: Props) => {
+  const [severity, setSeverity] = useState<"success" | "error" | undefined>(
+    undefined
+  );
+  const [isSnackBarOpen, setIsSnackBarOpen] = useState<boolean>(false);
+  const [snackBarText, setSnackBarText] = useState<string | undefined>(
+    undefined
+  );
+  const queryClient = useQueryClient();
+  const { mutate } = useNgsDestroy();
+
+  const handleSubmit = (ngId: number) => {
+    mutate(ngId, {
+      onSuccess: () => {
+        queryClient.invalidateQueries(`member/ngs`);
+        setSeverity("success");
+        setIsSnackBarOpen(true);
+        setSnackBarText("NGメモの削除に成功しました！");
+      },
+      onError: () => {
+        setSeverity("error");
+        setIsSnackBarOpen(true);
+        setSnackBarText("NGメモの削除に失敗しました");
+      },
+    });
+  };
+
   return (
     <>
-      <ListItemText>
+      <ListItemText style={{ width: "100%" }}>
         NGメモ：
-        <List dense>
+        <List dense style={{ width: "100%" }}>
           {props.response.map((ng_category, index) => (
             <Fragment key={index}>
               <ListSubheader disableSticky={true}>
                 {ng_category.categoryName}
               </ListSubheader>
               <ListItem>
-                <List dense>
+                <List dense style={{ width: "100%" }}>
                   {ng_category.ngs.map((ng, index) => (
                     <ListItem key={index}>
                       <ListItemAvatar>
@@ -51,6 +82,17 @@ export const NgMemoCollection = (props: Props) => {
                           ng.updatedAt
                         ).toLocaleDateString()}`}
                       ></ListItemText>
+                      <IconButton
+                        edge="end"
+                        aria-label="remove"
+                        onClick={() => {
+                          if (window.confirm("本当にNGメモを削除しますか？")) {
+                            handleSubmit(ng.id);
+                          }
+                        }}
+                      >
+                        <Delete />
+                      </IconButton>
                     </ListItem>
                   ))}
                 </List>
@@ -59,6 +101,13 @@ export const NgMemoCollection = (props: Props) => {
           ))}
         </List>
       </ListItemText>
+      <Snackbar
+        open={isSnackBarOpen}
+        autoHideDuration={5000}
+        onClose={() => setIsSnackBarOpen(false)}
+      >
+        <Alert severity={severity}>{snackBarText}</Alert>
+      </Snackbar>
     </>
   );
 };
