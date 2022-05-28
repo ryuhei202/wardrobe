58c433db953705afe2b41f85bc542e4908cd0eee
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-05-28 17:44:18 +0900

    feat: 体型情報に関連するAPIレスポンスの型を実装

---
+++ src/model/api/request/styling/member_size/UpdateSizes.ts
@@ -0,0 +1,20 @@
+export interface UpdateSizes {
+  height?: number;
+  weight?: number;
+  size_tops_admin?: number;
+  size_jacket_admin?: string;
+  size_drop_size_admin?: string;
+  shoulder?: number;
+  shoulder_jacket?: number;
+  bust?: number;
+  bust_jacket?: number;
+  length_top?: number;
+  length_top_jacket?: number;
+  length_arm?: number;
+  length_arm_jacket?: number;
+  waist?: number;
+  hip?: number;
+  round_leg?: number;
+  length_leg?: number;
+  round_calf?: number;
+}
---
+++ src/model/api/response/styling/member_size/MemberSizeSizeChoiceResponse.ts
@@ -0,0 +1,6 @@
+export interface MemberSizeSizeChoiceResponse {
+  size_tops_choices: string[];
+  size_bottoms_choices: string[];
+  size_jacket_choices: string[];
+  size_drop_size_choices: string[];
+}
