69d67f2a56c1acc8a67717dc42ef1fbe8c7f26ad
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-05-31 17:24:33 +0900

    feat: コーディネート一覧を取得するapi hooksを実装

---
+++ src/hooks/api/UseCoordinatesIndex.ts
@@ -0,0 +1,34 @@
+import {
+  QueryObserverResult,
+  RefetchOptions,
+  RefetchQueryFilters,
+} from "react-query";
+import { CoordinateIndexResponse } from "../../model/api/response/styling/coordinate/CoordinateIndexResponse";
+import { useGetRequest } from "./UseGetRequest";
+
+type CoordinatesIndex = {
+  readonly data?: CoordinateIndexResponse;
+  readonly error: Error | null;
+  readonly refetch: <TPageData>(
+    options?: RefetchOptions & RefetchQueryFilters<TPageData>
+  ) => Promise<QueryObserverResult<CoordinateIndexResponse, Error>>;
+  readonly isFetching: boolean;
+};
+
+type TCoordinatesIndexArg = {
+  chartId: number;
+};
+
+export const useCoordinatesIndex = ({
+  chartId,
+}: TCoordinatesIndexArg): CoordinatesIndex => {
+  const { data, error, refetch, isFetching } =
+    useGetRequest<CoordinateIndexResponse>(`kartes/${chartId}/coordinates`);
+
+  return {
+    data,
+    error,
+    refetch,
+    isFetching,
+  };
+};
