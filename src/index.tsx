0daed297bb0a3915ff7c366a11b696ee2eb503d6
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2022-06-13 15:59:54 +0900

    Revert "chore: エラーボタンを配置"
    This reverts commit a0ab46a70e031b0213c7c3693090ed8f15099bf3.

--- src/index.tsx
+++ src/index.tsx
@@ -18,13 +18,6 @@ ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Sentry.ErrorBoundary>
-        <button
-          onClick={() => {
-            throw new Error("エラー");
-          }}
-        >
-          エラー
-        </button>
         <App />
       </Sentry.ErrorBoundary>
     </BrowserRouter>
