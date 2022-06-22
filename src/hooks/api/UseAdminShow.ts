e1ef32ed1d718ba179c2617625f785cf094b7fbc
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-06-22 16:41:50 +0900

    feat: useAdminShowを実装

---
+++ src/hooks/api/UseAdminShow.ts
@@ -0,0 +1,24 @@
+import { useGetRequest } from "./UseGetRequest";
+
+export type TAdminShowResponse = {
+  readonly id: number;
+  readonly name: string;
+};
+
+type TAdminShow = {
+  readonly data?: TAdminShowResponse;
+  readonly error: Error | null;
+};
+
+type TAdminShowArg = {
+  readonly email: string;
+};
+
+export const useAdminShow = (params: TAdminShowArg): TAdminShow => {
+  const { data, error } = useGetRequest<TAdminShowResponse>("admin", params);
+
+  return {
+    data,
+    error,
+  };
+};
