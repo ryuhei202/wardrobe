9f57d58a053ea003729a02e6f2d6b906b6385ea1
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-06-22 16:43:42 +0900

    feat: AppBarにログインアイコンを追加

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,13 @@
 import { CropFree } from "@mui/icons-material";
-import { Button, Toolbar, Typography } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import AppBar from "@mui/material/AppBar/AppBar";
 import {
   StyledEngineProvider,
@@ -36,6 +44,7 @@ export const App = () => {
   const classes = useAppStyle();
   const [isQRCodeOpen, setIsQRCodeOpen] = useState(false);
   const [user, setUser] = useState<User | null>(null);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const queryClient = new QueryClient({
     defaultOptions: {
       queries: {
@@ -91,6 +100,45 @@ export const App = () => {
                       コーデピック
                     </Button>
                   </span>
+                  {user !== null && (
+                    <div>
+                      <IconButton
+                        size="small"
+                        aria-label="account of current user"
+                        aria-controls="profile-menu-appbar"
+                        aria-haspopup="true"
+                        onClick={() => setIsMenuOpen(true)}
+                        color="inherit"
+                      >
+                        <Avatar
+                          src={user.photoURL ?? undefined}
+                          alt={user.displayName ?? undefined}
+                        ></Avatar>
+                      </IconButton>
+                      <Menu
+                        id="profile-menu-appbar"
+                        anchorOrigin={{
+                          vertical: "top",
+                          horizontal: "right",
+                        }}
+                        transformOrigin={{
+                          vertical: "top",
+                          horizontal: "right",
+                        }}
+                        open={isMenuOpen}
+                        onClose={() => setIsMenuOpen(false)}
+                      >
+                        <MenuItem
+                          onClick={() => {
+                            setIsMenuOpen(false);
+                            signOut(getAuth());
+                          }}
+                        >
+                          ログアウト
+                        </MenuItem>
+                      </Menu>
+                    </div>
+                  )}
                 </Toolbar>
               </AppBar>
               <CordePickQRDialog
