94fe4cca724b24105ff5f14ab9209664286bca11
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-06-24 11:14:40 +0900

    refactor: 環境変数の切り替えにenv-vmdを使うよう修正

--- src/model/HostUrl.ts
+++ src/model/HostUrl.ts
@@ -1,6 +1,3 @@
 export const HostUrl = (): string => {
-  if (process.env.REACT_APP_ENV === "staging") {
-    return process.env.REACT_APP_HOST_URL_STAGING ?? "";
-  }
   return process.env.REACT_APP_HOST_URL ?? "";
 };
