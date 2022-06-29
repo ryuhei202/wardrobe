f7120f4797d3cd90b3338041eb2c678b1e7f895f
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-06-29 14:37:26 +0900

    feat: useCoordinateItemsIndexを実装

---
+++ src/model/api/response/styling/coordinateItem/CoordinateItemsIndexResponse.ts
@@ -0,0 +1,15 @@
+import { ImagePathResponse } from "../ImagePathResponse";
+import { PartSizeResponse } from "../karte/KartePartSizeResponse";
+
+export interface CoordinateItemResponse {
+  readonly id: number;
+  readonly imagePath: ImagePathResponse;
+  readonly categoryName: string;
+  readonly mainColorName: string;
+  readonly subColorName: string;
+  readonly brandName: string;
+  readonly patternName: string;
+  readonly size: string;
+  readonly partSizes: PartSizeResponse[];
+  readonly dropSize: string;
+}
