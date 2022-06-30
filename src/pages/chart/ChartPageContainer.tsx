e4cb1d5998e661984d24abc03f57a5deb51b35fa
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2022-06-30 17:29:46 +0900

    feat: ChartPageコンポーネントを実装

---
+++ src/pages/chart/ChartPageContainer.tsx
@@ -0,0 +1,42 @@
+import { CircularProgress, Typography } from "@mui/material";
+import { useContext, useEffect } from "react";
+import {
+  ChartIdContext,
+  MemberIdContext,
+  MemberShowContext,
+} from "../../components/context/provider/ContextProvider";
+import { useContextDefinedState } from "../../components/context/UseContextDefinedState";
+import { useKartesShow } from "../../hooks/api/UseKartesShow";
+import { useMembersShow } from "../../hooks/api/UseMembersShow";
+import { ChartPage } from "./ChartPage";
+
+export const ChartPageContainer = () => {
+  const chartId = useContextDefinedState(ChartIdContext);
+  const memberId = useContextDefinedState(MemberIdContext);
+  const { state: memberShowState, setter: setMemberShowContext } =
+    useContext(MemberShowContext);
+
+  const { data: karteShowData, error: karteShowError } = useKartesShow({
+    chartId,
+  });
+
+  const memberShowRes = useMembersShow({
+    memberId,
+  });
+
+  useEffect(() => {
+    if (
+      memberShowState === null ||
+      memberShowRes.data?.id !== memberShowState?.data?.id
+    ) {
+      setMemberShowContext(memberShowRes);
+    }
+  }, [memberShowRes, memberShowState, setMemberShowContext]);
+
+  if (!karteShowData || memberShowState === null)
+    return <CircularProgress sx={{ m: "auto" }} />;
+  if (karteShowError)
+    return <Typography sx={{ m: "auto" }}>{karteShowError.message}</Typography>;
+
+  return <ChartPage />;
+};
