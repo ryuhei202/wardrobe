6da55f286101ad3a9bff022972cefd7104c0aeef
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2022-11-18 16:28:41 +0900

    upgrade: フロントテスト実行環境を構築 <release/LEEAP-3554-wardrobeフロントエンドテスト構築v2>

---
+++ src/__tests__/header/ChartPickButton.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import { ChartPickButton } from "../../components/header/ChartPickButton";
+
+test("ChartPickButtonの表示がされる", () => {
+  render(<ChartPickButton onClick={() => {}} />);
+
+  const button = screen.getByText("カルテピック");
+  expect(button).toBeInTheDocument;
+});
