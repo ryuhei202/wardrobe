dc589a876968562b4a59ba2875f7b0c1ad3c64a6
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-02-07 13:02:54 +0900

    remove: テストの消去 <release/LEEAP-3837-wordroabnpmアップデート>

--- src/__tests__/components/header/ChartPickButton.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import { ChartPickButton } from "../../../components/header/ChartPickButton";
-
-test("ChartPickButtonの表示がされる", () => {
-  render(<ChartPickButton onClick={() => {}} />);
-
-  const button = screen.getByText("カルテピック");
-  expect(button).toBeInTheDocument;
-});
