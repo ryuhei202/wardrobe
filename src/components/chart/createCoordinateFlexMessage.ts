52feb2aaf373b7558ed559ae797858e655a6cf48
杉森康太 <k-sugimori@kiizan-kiizan.co.jp>
2023-02-22 12:18:49 +0900

    feat: FlexMessageを作成する関数を実装 <feature/3912/LEEAP-3925-wr送信front-api-hooksの実装>

---
+++ src/components/chart/createCoordinateFlexMessage.ts
@@ -0,0 +1,79 @@
+type TProps = {
+  coordinateDescription: string;
+  items: {
+    imageUrl: string;
+    colorText: string;
+  }[];
+  isFirstTransmit: boolean;
+};
+
+export const createCoordinateFlexMessage = ({
+  coordinateDescription,
+  items,
+  isFirstTransmit,
+}: TProps) => {
+  const flexMessage = [];
+  flexMessage.push({
+    type: "text",
+    text: isFirstTransmit
+      ? "診断フォームにご回答いただきありがとうございます！\nフォームの回答内容を元に早速コーデをお作りしましたのでご確認お願いします！"
+      : "再度コーデをお作りしましたのでご確認お願いします！",
+    sender: true,
+  });
+
+  flexMessage.push({
+    type: "carousel",
+    contents: items.map((item) => {
+      return {
+        type: "bubble",
+        hero: {
+          type: "image",
+          url: item.imageUrl,
+          aspectRatio: "2:3",
+          size: "full",
+        },
+        body: {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: item.colorText,
+              size: "md",
+              color: "#4C5257",
+              weight: "bold",
+              align: "center",
+            },
+          ],
+          paddingAll: "md",
+          backgroundColor: "#E8E7DF",
+        },
+      };
+    }),
+  });
+
+  flexMessage.push({
+    type: "text",
+    text: `${coordinateDescription}\n\n私としては今回の利用シーンと印象で、このようなコーデを考えていますがいかがでしょうか？`,
+    quickReply: {
+      items: [
+        {
+          type: "action",
+          action: {
+            type: "message",
+            text: "こちらのコーデでお願いします！",
+          },
+        },
+        {
+          type: "action",
+          action: {
+            type: "message",
+            text: "コーデの変更を依頼したいです！",
+          },
+        },
+      ],
+    },
+  });
+
+  return JSON.stringify(flexMessage);
+};
