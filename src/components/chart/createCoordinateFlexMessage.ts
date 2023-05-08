5fb437f9dbc3ae15c37c8b0b59b0158cf663ca35
津守勇汰 <y-tsumori@kiizan-kiizan.co.jp>
2023-05-08 18:25:22 +0900

    feat: コーデ提案のflexメッセージに簡易ヒアリング内容を追加 <release/LEEAP-4355-コーデ提案のflex文でヒアリング内容が確認できる>

--- src/components/chart/createCoordinateFlexMessage.ts
+++ src/components/chart/createCoordinateFlexMessage.ts
@@ -1,15 +1,18 @@
+import { SimplifiedHearingsShowResponse } from "../../model/api/response/styling/simplifiedHearing/SimplifiedHearingsShowResponse";
 import { TCoordinateItem } from "../../model/coordinateItem/TCoordinateItem";
 
 type TArgs = {
   descriptionText: string;
   coordinateItems: TCoordinateItem[];
   isFirstTransmit: boolean;
+  simplifiedHearing?: SimplifiedHearingsShowResponse;
 };
 
 export const createCoordinateFlexMessage = ({
   descriptionText,
   coordinateItems,
   isFirstTransmit,
+  simplifiedHearing,
 }: TArgs) => {
   const flexMessage: any[] = [
     {
@@ -20,47 +23,135 @@ export const createCoordinateFlexMessage = ({
     },
   ];
 
+  const simplifiedHearingContent = {
+    type: "bubble",
+    body: {
+      type: "box",
+      layout: "vertical",
+      contents: [
+        {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "意識する相手",
+              size: "sm",
+              color: "#4C5257",
+            },
+            {
+              type: "text",
+              text: simplifiedHearing?.target || "未回答",
+              size: "md",
+              weight: "bold",
+              wrap: true,
+              color: "#4C5257",
+            },
+          ],
+        },
+        {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "シーン",
+              size: "sm",
+              color: "#4C5257",
+            },
+            {
+              type: "text",
+              text: simplifiedHearing?.scene || "未回答",
+              size: "md",
+              weight: "bold",
+              wrap: true,
+              color: "#4C5257",
+            },
+          ],
+        },
+        {
+          type: "box",
+          layout: "vertical",
+          contents: [
+            {
+              type: "text",
+              text: "印象",
+              size: "sm",
+              color: "#4C5257",
+            },
+            {
+              type: "text",
+              text: simplifiedHearing?.impression || "未回答",
+              size: "md",
+              weight: "bold",
+              wrap: true,
+              color: "#4C5257",
+            },
+          ],
+        },
+      ],
+      justifyContent: "space-around",
+      alignItems: "flex-start",
+    },
+    styles: {
+      body: {
+        backgroundColor: "#E8E7DF",
+      },
+    },
+  };
+
   flexMessage.push({
     type: "flex",
     altText: "【コーデ写真】",
     contents: {
       type: "carousel",
-      contents: coordinateItems.map((item) => {
-        const bodyContents = [];
-        if (item.isChangeItem) {
+      contents: [
+        simplifiedHearingContent,
+        ...coordinateItems.map((item) => {
+          const bodyContents = [];
+          if (item.isChangeItem) {
+            bodyContents.push({
+              type: "text",
+              text: "[チェンジアイテム]",
+              size: "md",
+              color: "#4C5257",
+              align: "center",
+            });
+          }
           bodyContents.push({
             type: "text",
-            text: "[チェンジアイテム]",
+            text: `${item.itemInfo.categoryName} / ${item.itemInfo.mainColorName}`,
             size: "md",
             color: "#4C5257",
+            weight: "bold",
             align: "center",
           });
-        }
-        bodyContents.push({
-          type: "text",
-          text: `${item.itemInfo.categoryName} / ${item.itemInfo.mainColorName}`,
-          size: "md",
-          color: "#4C5257",
-          weight: "bold",
-          align: "center",
-        });
-        return {
-          type: "bubble",
-          hero: {
-            type: "image",
-            url: item.itemInfo.imagePath.large,
-            aspectRatio: "2:3",
-            size: "xl",
-          },
-          body: {
-            type: "box",
-            layout: "vertical",
-            contents: bodyContents,
-            paddingAll: "md",
-            backgroundColor: "#E8E7DF",
-          },
-        };
-      }),
+          return {
+            type: "bubble",
+            hero: {
+              type: "image",
+              url: item.itemInfo.imagePath.large,
+              aspectRatio: "2:3",
+              size: "xl",
+            },
+            body: {
+              type: "box",
+              layout: "vertical",
+              contents: bodyContents,
+              paddingAll: "md",
+              justifyContent: "center",
+            },
+            styles: {
+              hero: {
+                backgroundColor: "#E8E7DF",
+              },
+              body: {
+                backgroundColor: "#E8E7DF",
+              },
+            },
+          };
+        }),
+      ],
     },
   });
 
