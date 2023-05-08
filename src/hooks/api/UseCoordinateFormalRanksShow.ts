ba7a0c3d9b96989bab136c9ab56567af3f7d04f8
高垣龍平 <r-takagaki@kiizan-kiizan.co.jp>
2023-05-08 12:18:53 +0900

    feat: showアクションのレスポンスを取得するフックスを作成 <feature/4322/LEEAP-4329-frontwrキレイ度登録usecoordinatefom>

---
+++ src/hooks/api/UseCoordinateFormalRanksShow.ts
@@ -0,0 +1,21 @@
+import { CoordinateFormalRanksShowResponse } from "../../model/api/response/styling/coordinateFormalRank/CoordinateFormalRanksShowResponse";
+import { useGetRequest } from "./UseGetRequest";
+
+type useCoordinateFormalRanksShowArg = {
+  coordinateId: number;
+};
+
+export const useCoordinateFormalRanksShow = ({
+  coordinateId,
+}: useCoordinateFormalRanksShowArg) => {
+  const { data, error, refetch } =
+    useGetRequest<CoordinateFormalRanksShowResponse>(
+      `coordinates/${coordinateId}/coordinate_formal_ranks`
+    );
+
+  return {
+    data,
+    error,
+    refetch,
+  };
+};
