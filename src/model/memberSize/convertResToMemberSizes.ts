cbd47341a98df1c58139bff97330298e379deea6
宮本和輝 <k-miyamoto@kiizan-kiizan.co.jp>
2023-05-18 17:37:28 +0900

    feat: choicesを使えるようにした <feature/4374/LEEAP-4405-wr体型表示memberbodyshape.tsx作成>

--- src/model/memberSize/convertResToMemberSizes.ts
+++ src/model/memberSize/convertResToMemberSizes.ts
@@ -21,6 +21,7 @@ export const convertResToMemberSizes = (
       bmi,
       height,
       weight,
+      choices,
     } = response;
 
     return {
@@ -35,6 +36,7 @@ export const convertResToMemberSizes = (
       bmi,
       height,
       weight,
+      choices,
     };
   };
 
