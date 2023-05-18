489c2c849ead896a8c618d10aac2de0d7b59d0f0
宮本和輝 <k-miyamoto@kiizan-kiizan.co.jp>
2023-05-18 17:51:52 +0900

    feat: MemberBodyShape.tsxの新規作成 <feature/4374/LEEAP-4405-wr体型表示memberbodyshape.tsx作成>

---
+++ src/components/memberSize/MemberBodyShape.tsx
@@ -0,0 +1,78 @@
+import {
+  Table,
+  TableBody,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+} from "@mui/material";
+import { StyledTableCell, StyledTableRow } from "./MemberSizeTable";
+
+type TProps = {
+  readonly shapeWaist: string;
+  readonly shoulder: string;
+  readonly hip: string;
+  readonly bust: string;
+};
+
+export const MemberBodyShape = ({
+  bust,
+  shapeWaist,
+  shoulder,
+  hip,
+}: TProps) => {
+  return (
+    <TableContainer>
+      <Table size="small">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell>体型</StyledTableCell>
+            <StyledTableCell>タイプ</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          <StyledTableRow>
+            <StyledTableCell>
+              <Typography fontWeight={"bold"} variant={"subtitle2"} p={1}>
+                肩幅のタイプ
+              </Typography>
+            </StyledTableCell>
+            <StyledTableCell>
+              <Typography>{shoulder}</Typography>
+            </StyledTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledTableCell>
+              <Typography fontWeight={"bold"} variant={"subtitle2"} p={1}>
+                胸板のタイプ
+              </Typography>
+            </StyledTableCell>
+            <StyledTableCell>
+              <Typography>{bust}</Typography>
+            </StyledTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledTableCell>
+              <Typography fontWeight={"bold"} variant={"subtitle2"} p={1}>
+                お腹周りのタイプ
+              </Typography>
+            </StyledTableCell>
+            <StyledTableCell>
+              <Typography>{shapeWaist}</Typography>
+            </StyledTableCell>
+          </StyledTableRow>
+          <StyledTableRow>
+            <StyledTableCell>
+              <Typography fontWeight={"bold"} variant={"subtitle2"} p={1}>
+                ヒップのタイプ
+              </Typography>
+            </StyledTableCell>
+            <StyledTableCell>
+              <Typography>{hip}</Typography>
+            </StyledTableCell>
+          </StyledTableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
