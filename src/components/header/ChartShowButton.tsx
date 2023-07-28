cb08c9b45bd56f52bcce51fc3f471f7821b203ef
江崎広太 <k-esaki@kiizan-kiizan.co.jp>
2023-07-28 14:42:15 +0900

    fix: オーネットはカルテ画面に遷移するボタンを非表示にする <feature/4403/LEEAP-4686-onet-wr-front-rightdrawercont>

--- src/components/header/ChartShowButton.tsx
+++ src/components/header/ChartShowButton.tsx
@@ -1,32 +1,35 @@
-import { Button } from "@mui/material";
 import ArticleIcon from "@mui/icons-material/Article";
+import { Button } from "@mui/material";
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import {
   ChartIdContext,
   MemberIdContext,
 } from "../context/provider/ContextProvider";
 
 export const ChartShowButton = () => {
+  const { pathname } = useLocation();
   const memberId = useContext(MemberIdContext).state;
   const chartId = useContext(ChartIdContext).state;
 
   return (
     <div style={{ flexGrow: 1 }}>
-      <Button
-        variant="contained"
-        color="primary"
-        disableElevation
-        component={Link}
-        to={{
-          pathname: "/chart",
-          search: `?memberId=${memberId}&chartId=${chartId}`,
-        }}
-        startIcon={<ArticleIcon />}
-        sx={{ fontWeight: "bold" }}
-      >
-        カルテ
-      </Button>
+      {!pathname.includes("rentals") && (
+        <Button
+          variant="contained"
+          color="primary"
+          disableElevation
+          component={Link}
+          to={{
+            pathname: "/chart",
+            search: `?memberId=${memberId}&chartId=${chartId}`,
+          }}
+          startIcon={<ArticleIcon />}
+          sx={{ fontWeight: "bold" }}
+        >
+          カルテ
+        </Button>
+      )}
     </div>
   );
 };
