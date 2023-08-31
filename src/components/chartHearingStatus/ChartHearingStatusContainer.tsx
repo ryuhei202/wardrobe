336ca3dfe4376e10c77c7370c9611c5c350b465a
kojikojiXX <kouji.nohanoha@gmail.com>
2023-08-31 18:31:16 +0900

    feat: ChartHearingStatusContainerの作成 <feature/4515/LEEAP-4767-wardrobeの修正>

---
+++ src/components/chartHearingStatus/ChartHearingStatusContainer.tsx
@@ -0,0 +1,24 @@
+import { CircularProgress, Typography } from "@mui/material";
+import { useKarteHearingStatusShow } from "../../hooks/api/UseKarteHearingStatusShow";
+import { ChartHearingStatus } from "./ChartHearingStatus";
+
+type TProps = {
+  chartId: number;
+};
+
+export const ChartHearingStatusContainer = ({ chartId }: TProps) => {
+  const { data, error } = useKarteHearingStatusShow({ chartId });
+
+  console.log(data);
+
+  if (error) return <Typography>{error.message}</Typography>;
+  if (!data) return <CircularProgress />;
+
+  return (
+    <ChartHearingStatus
+      chartId={chartId}
+      currentStatus={data.currentStatus}
+      nextStatuses={data.nextStatuses}
+    />
+  );
+};
