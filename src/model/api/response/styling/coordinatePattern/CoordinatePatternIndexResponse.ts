68d89c88e91a495205ea9ab90bb1c3ebd684dc4e
kojikojiXX <kouji.nohanoha@gmail.com>
2023-09-22 16:58:21 +0900

    fix: apiを変更していたが、元に戻したので、削除 <feature/LEEAP-4855-wardrobe着こなしアドバイスを入れてなくても、コーデ>

--- src/model/api/response/styling/coordinatePattern/CoordinatePatternIndexResponse.ts
+++ src/model/api/response/styling/coordinatePattern/CoordinatePatternIndexResponse.ts
@@ -1,9 +1,7 @@
-import { TItem } from "../../../../selecting/TItem";
 import { CoordinateAdviceCategoryResponse } from "./CoordinateAdviceCategoryResponse";
 import { CoordinatePattern } from "./CoordinatePattern";
 
 export interface CoordinatePatternIndexResponse {
   readonly adviceCategories: CoordinateAdviceCategoryResponse[];
   readonly selectedCoordinatePatterns: CoordinatePattern[];
-  readonly coordinateItems: TItem[];
 }
